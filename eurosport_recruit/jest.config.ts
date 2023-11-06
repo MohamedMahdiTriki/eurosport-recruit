83bad6c81f787f2447a0426e53192b596c1f666c
mahdi_triki <med.mahdi.triki@gmail.com>
2023-11-06 16:05:47 +0100

    adding untracked files

--- eurosport_recruit/jest.config.ts
+++ eurosport_recruit/jest.config.ts
@@ -1,7 +1,19 @@
-import type { Config } from 'jest'
+// import type { Config } from 'jest'
 
-const config: Config = {
-    verbose: true,
+// const config: Config = {
+//     verbose: true,
+// }
+
+// export default config
+
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+        // process `*.tsx` files with `ts-jest`
+    },
+    moduleNameMapper: {
+        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
+    },
 }
-
-export default config
